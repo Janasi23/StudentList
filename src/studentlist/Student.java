44540e76b371b53de7164bf98ec32dad94fd6f62
Janasi Rajput <rajputja@sheridancollge.ca>
2024-09-16 09:35:43 -0400

    Getter setter are added.

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -21,5 +21,14 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
    
 }
