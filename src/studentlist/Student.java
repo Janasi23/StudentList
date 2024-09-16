d34231319d7a518b4ca970b1f38be228ef428452
Janasi Rajput <rajputja@sheridancollge.ca>
2024-09-16 09:30:53 -0400

    Student.java updated with one file added

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -10,8 +10,9 @@ package studentlist;
  */
 public class Student {
     private String name;
-   /* private String studentID;
-    private String address;*/
+   private String studentID;
+    private String address;
+    
 
     public String getName() {
         return name;
