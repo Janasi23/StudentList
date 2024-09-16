24990025f54db67fc89b8ceedc4452b08c6c30a3
Janasi Rajput <rajputja@sheridancollge.ca>
2024-09-16 10:40:16 -0400

    commited

---
+++ src/PartTimeStudent.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rajpu
+ */
+public class PartTimeStudent extends Student{
+    private int NumCourses;
+
+    public int getNumCourses() {
+        return NumCourses;
+    }
+
+    public void setNumCourses(int NumCourses) {
+        this.NumCourses = NumCourses;
+    }
+    
+    
+}
